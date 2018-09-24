27792fbb74b891f815dd06fd91a1964b4ce7b79d
Magesh <tkmagesh77@gmail.com>
2018-09-24 15:13:20 +0530

    app server for calculator

--- 06-appserver/server.js
+++ 06-appserver/server.js
@@ -1,19 +1,26 @@
 var http = require('http'),
 	fs = require('fs'),
 	path = require('path'),
-	url = require('url');
+	url = require('url'),
+	querystring = require('querystring'),
+	calculator = require('./calculator');
 
 var server = http.createServer(function(req, res){
 	console.log('a new connection is established for ', req.url);
 	var urlObj = url.parse(req.url);
-	var resourceName = urlObj.pathname === '/' ? 'index.html' : urlObj.pathname,
-		resourcePath = path.join(__dirname, resourceName);
-	if (!fs.existsSync(resourcePath)){
+	if (urlObj.pathname !== '/calculator'){
 		res.statusCode = 404;
 		res.end();
 		return;
 	}
-	fs.createReadStream(resourcePath).pipe(res);
+	var data = querystring.parse(urlObj.query),
+		op = data.op,
+		n1 = parseInt(data.n1),
+		n2 = parseInt(data.n2),
+		result = calculator[op](n1, n2);
+
+	res.write(result.toString());
+	res.end();
 });
 
 server.listen(8080);
