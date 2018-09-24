88d95b864f96a27b84fa70bb5f9d2dde691e92a5
Magesh <tkmagesh77@gmail.com>
2018-09-24 15:00:33 +0530

    web server - complete

--- 05-webserver/server.js
+++ 05-webserver/server.js
@@ -1,8 +1,17 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs'),
+	path = require('path');
 
 var server = http.createServer(function(req, res){
-	res.write('<h1>Welcome to Node.js</h1>');
-	res.end();
+	console.log('a new connection is established for ', req.url);
+	var resourceName = req.url === '/' ? 'index.html' : req.url,
+		resourcePath = path.join(__dirname, resourceName);
+	if (!fs.existsSync(resourcePath)){
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
+	fs.createReadStream(resourcePath).pipe(res);
 });
 
 server.listen(8080);
