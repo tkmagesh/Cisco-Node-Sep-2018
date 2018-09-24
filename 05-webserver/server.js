f35195cfb6f031ad515df25a82a84836bd262386
Magesh <tkmagesh77@gmail.com>
2018-09-24 14:37:44 +0530

    web server - part - 1

---
+++ 05-webserver/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+	res.write('<h1>Welcome to Node.js</h1>');
+	res.end();
+});
+
+server.listen(8080);
+
+console.log('server listening on 8080!');
