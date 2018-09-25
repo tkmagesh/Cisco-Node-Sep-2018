1173880e7620960d619d7672d4ab62338ffb895d
Magesh <tkmagesh77@gmail.com>
2018-09-25 11:38:30 +0530

    moving data parsing logic to data parser

--- 08-webappserver-refactored/serveCalculator.js
+++ 08-webappserver-refactored/serveCalculator.js
@@ -2,8 +2,8 @@ var querystring = require('querystring'),
 	calculator = require('./calculator');
 
 module.exports = function(req, res, next){
-	if (req.urlObj.pathname === '/calculator' && req.method === 'GET'){
-		var data = querystring.parse(req.urlObj.query),
+	if (req.urlObj.pathname === '/calculator'){
+		var data = req.method === 'GET' ? req.queryData : req.bodyData,
 			op = data.op,
 			n1 = parseInt(data.n1),
 			n2 = parseInt(data.n2),
@@ -12,22 +12,6 @@ module.exports = function(req, res, next){
 		res.write(result.toString());
 		res.end();
 		next();
-	} else if (req.urlObj.pathname === '/calculator' && req.method === 'POST'){
-		var rawData = '';
-		req.on('data', function(chunk){
-			rawData += chunk;
-		});
-		req.on('end', function(){
-			var data = querystring.parse(rawData),
-				op = data.op,
-				n1 = parseInt(data.n1),
-				n2 = parseInt(data.n2),
-				result = calculator[op](n1, n2);
-
-			res.write(result.toString());
-			res.end();
-			next();
-		});
 	} else {
 		next();
 	}
