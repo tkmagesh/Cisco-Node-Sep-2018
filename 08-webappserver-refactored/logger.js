d68f5efb012db0e7d859f9820bbb579e5f3a6d68
Magesh <tkmagesh77@gmail.com>
2018-09-25 12:25:01 +0530

    using nam

--- 08-webappserver-refactored/logger.js
+++ 08-webappserver-refactored/logger.js
@@ -1,9 +1,11 @@
+var chalk = require('chalk');
+
 module.exports = function(req, res, next){
 	var startTime = new Date();
 	res.on('finish', function(){
 		var endTime = new Date();
 		var elapsedTime = endTime - startTime;
-		console.log(req.method + '\t' + res.statusCode + ' ' + req.urlObj.pathname + ' ' + elapsedTime + 'ms');
+		console.log(chalk.blue(req.method) + '\t' + chalk.red(res.statusCode) + ' - ' + chalk.cyan(req.urlObj.pathname) + ' ' + elapsedTime + 'ms');
 	});
 	next();
 };
