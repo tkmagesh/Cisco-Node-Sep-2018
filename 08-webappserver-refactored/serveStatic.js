e3ae4977b1f62c80574aac20fe6846dc6eb8c10f
Magesh <tkmagesh77@gmail.com>
2018-09-25 09:22:01 +0530

    webappserver - refactored - assignment

---
+++ 08-webappserver-refactored/serveStatic.js
@@ -0,0 +1,22 @@
+var fs = require('fs'),
+	path = require('path');
+
+var staticExtns = ['.html', '.css', '.js', '.png', '.jpg', '.ico', '.xml', '.txt'];
+
+function isStatic(resourceName){
+	return staticExtns.indexOf(path.extname(resourceName)) >= 0;
+}
+
+module.exports = function(req, res){
+	var resourceName = req.urlObj.pathname === '/' ? 'index.html' : req.urlObj.pathname;
+	if (isStatic(resourceName)){
+		var resourcePath = path.join(__dirname, resourceName);
+
+		if (!fs.existsSync(resourcePath)){
+			res.statusCode = 404;
+			res.end();
+			return;
+		}
+		fs.createReadStream(resourcePath).pipe(res);
+	}
+}
